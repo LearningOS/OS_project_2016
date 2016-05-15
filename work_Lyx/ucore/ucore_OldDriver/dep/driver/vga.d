5225e1776477c1c9be11dd4d2471b5726af4314f
AntNTmn <JFLFY2255@gmail.com>
2016-05-15 22:41:14 +0800

    work_Lyx_delete

--- work_Lyx/ucore/ucore_OldDriver/dep/driver/vga.d
+++
@@ -1,1 +0,0 @@
-obj/driver/vga.o dep/driver/vga.d: kern/driver/vga.c
--- work_Lyx/ucore/ucore_OldDriver/dep/fetchrun/fetchrun.d
+++
@@ -1,9 +0,0 @@
-obj/fetchrun/fetchrun.o dep/fetchrun/fetchrun.d: kern/fetchrun/fetchrun.c \
- kern/include/defs.h kern/include/thumips.h kern/driver/intr.h \
- kern/include/asm/mipsregs.h kern/fetchrun/fetchrun.h kern/fs/file.h \
- kern/fs/fs.h kern/mm/mmu.h kern/include/mips_vm.h kern/sync/sem.h \
- kern/include/atomic.h kern/debug/assert.h kern/sync/wait.h \
- kern/include/list.h kern/process/proc.h kern/trap/trap.h \
- kern/trap/mips_trapframe.h kern/mm/memlayout.h kern/sync/sync.h \
- kern/schedule/sched.h kern/libs/stdio.h kern/include/stdarg.h \
- kern/mm/kmalloc.h
